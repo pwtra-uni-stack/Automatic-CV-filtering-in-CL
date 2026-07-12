8a27f1ba800e65ab2f7b2c124fece1ec2b7438b8
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-12 21:53:29 +0700

    Implement DuplicateCVException class

--- src/exception/DuplicateCVException.h
+++ src/exception/DuplicateCVException.h
@@ -5,4 +5,24 @@
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_DUPLICATECVEXCEPTION_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_DUPLICATECVEXCEPTION_H
 
+#include <exception>
+#include <string>
+
+class DuplicateCVException:public std::exception{
+
+private:
+
+    std::string msg;
+
+public:
+
+    DuplicateCVException(std::string m):msg(m){}
+
+    const char* what()const noexcept override{
+
+        return msg.c_str();
+    }
+
+};
+
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_DUPLICATECVEXCEPTION_H
