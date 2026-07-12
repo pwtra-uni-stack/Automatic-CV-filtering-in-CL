7ad29106266e8dfb26b73bebb7d88f9132f9db53
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-12 11:16:01 +0700

    Add EducationFilter class for CV filtering

--- src/model/EducationFilter.h
+++ src/model/EducationFilter.h
@@ -4,5 +4,21 @@
 
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_EDUCATIONFILTER_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_EDUCATIONFILTER_H
+#include "CVFilter.h"
+#include <string>
 
+class EducationFilter : public CVFilter {
+public:
+    std::string required_edu;
+
+    //Hàm khởi tạo gán tiêu chuẩn trình độ học vấn cần lọc
+    EducationFilter(std::string edu) {
+        required_edu = edu;
+    }
+
+    //Ghi đè lại hàm match để so sánh chuỗi trình độ học vấn
+    bool match(CV cv) override {
+        return cv.education == required_edu;
+    }
+};
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_EDUCATIONFILTER_H
