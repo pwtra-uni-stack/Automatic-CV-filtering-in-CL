2dd43c7b9ae83cef523c4916e4df1cd6901a2554
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-12 21:47:36 +0700

    Add ScanStrategy interface for scanning CV objects

--- src/scanner/ScanStrategy.h
+++ src/scanner/ScanStrategy.h
@@ -5,4 +5,10 @@
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_SCANSTRATEGY_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_SCANSTRATEGY_H
 
+class ScanStrategy {
+public:
+    virtual bool scan(const CV& cv)=0;
+    virtual ~ScanStrategy(){}
+};
+
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_SCANSTRATEGY_H
