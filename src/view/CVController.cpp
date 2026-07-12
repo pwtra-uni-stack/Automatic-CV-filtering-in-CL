6eaf3b5434c0391e606722473a44fe6e97daa9b2
pwtra-uni-stack <pwtra.pj@gmail.com>
2026-07-12 22:03:34 +0700

    Fix folder structure: move CVController.cpp and main.cpp to correct locations

--- src/view/CVController.cpp
+++
@@ -1,9 +0,0 @@
-#include "CVController.h"
-#include <iostream>
-
-void CVController::run() {
-    std::cout << "CV_Auto started!" << std::endl;
-}
-
-void CVController::handleLoad() { /* TODO */ }
-void CVController::handleViewAll() { /* TODO */ }
