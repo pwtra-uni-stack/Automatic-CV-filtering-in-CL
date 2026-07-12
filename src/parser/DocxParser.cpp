6056091d38c44af36f8d0ab6e1c181b02d9f755a
nhphuongdung2208-create <nhphuongdung.2208@gmail.com>
2026-07-12 20:09:24 +0700

    Add files via upload

---
+++ src/parser/DocxParser.cpp
@@ -0,0 +1,21 @@
+#include "DocxParser.h"
+#include <fstream>
+#include <iostream>
+
+std::string DocxParser::parse(std::string duong_dan_file) {
+    std::ifstream file(duong_dan_file);
+    if (file.is_open() == false) {
+        std::cout << "Loi: Khong mo duoc file CV tai: " << duong_dan_file << std::endl;
+        return "";
+    }
+
+    std::string noi_dung_tong = "";
+    std::string dong_chu;
+    
+    while (std::getline(file, dong_chu)) {
+        noi_dung_tong += dong_chu + "\n";
+    }
+    
+    file.close();
+    return noi_dung_tong;
+}
