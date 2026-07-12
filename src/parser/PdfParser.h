00376d5631136622f561e1992f43046bf393ea26
nhphuongdung2208-create <nhphuongdung.2208@gmail.com>
2026-07-12 22:51:25 +0700

    Update PdfParser.h

--- src/parser/PdfParser.h
+++ src/parser/PdfParser.h
@@ -4,5 +4,10 @@
 
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_PDFPARSER_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_PDFPARSER_H
+#include "DocumentParser.h"
 
+class PdfParser : public DocumentParser {
+public:
+    std::string parse(std::string duong_dan_file) override;
+};
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_PDFPARSER_H
