b03da77515a02872f308de7714826f6b003a49e9
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-12 11:14:12 +0700

    Create CVFilter base class for filter inheritance
    Added base class CVFilter with pure virtual match method.

--- src/model/CVFilter.h
+++ src/model/CVFilter.h
@@ -4,5 +4,15 @@
 
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_CVFILTER_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_CVFILTER_H
+#include "CV.h"
 
+//Lớp cha cơ sở (Base class) thiết lập khuôn mẫu cho tính chất kế thừa & đa hình
+class CVFilter {
+public:
+    //Hàm thuần ảo = 0 bắt buộc các lớp con bộ lọc phải ghi dè định nghĩa lại điều kiện lọc
+    virtual bool match(CV cv) = 0;
+
+    //Destructor ảo bảo đảm giải phóng bộ nhớ sạch sẽ, an toàn khi làm việc với con trỏ OOP
+    virtual ~CVFilter() {}
+};
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_CVFILTER_H
