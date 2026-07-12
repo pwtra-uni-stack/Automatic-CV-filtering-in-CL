7be1cc4df6f2fd00fda92e83d5d790aa87629d2f
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-12 21:48:02 +0700

    Implement scan method in BasicScan class

--- src/scanner/BasicScan.h
+++ src/scanner/BasicScan.h
@@ -5,4 +5,25 @@
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_BASICSCAN_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_BASICSCAN_H
 
+class BasicScan : public ScanStrategy{
+public:
+
+    bool scan(const CV& cv) override{
+
+        if(cv.candidate.name.empty())
+            return false;
+
+        if(cv.candidate.email.empty())
+            return false;
+
+        if(cv.skills.empty())
+            return false;
+
+        if(cv.raw_text.length()<30)
+            return false;
+
+        return true;
+    }
+};
+
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_BASICSCAN_H
