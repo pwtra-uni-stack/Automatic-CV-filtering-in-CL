8a7c7ebd4986682de8060e96fe6858e55f703ed6
nhphuongdung2208-create <nhphuongdung.2208@gmail.com>
2026-07-12 20:07:30 +0700

    Update DocxParser.h

--- src/parser/DocxParser.h
+++ src/parser/DocxParser.h
@@ -4,5 +4,8 @@
 
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_DOCXPARSER_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_DOCXPARSER_H
-
+class DocxParser : public DocumentParser {
+public:
+    std::string parse(std::string duong_dan_file) override;
+};
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_DOCXPARSER_H
