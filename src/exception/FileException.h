6499bb4e12b9f9d0980e3a7c01c40ff1a7bd9f7e
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-12 21:52:42 +0700

    Update FileException.h

--- src/exception/FileException.h
+++ src/exception/FileException.h
@@ -5,4 +5,24 @@
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_FILEEXCEPTION_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_FILEEXCEPTION_H
 
+#include <exception>
+#include <string>
+
+class FileException:public std::exception{
+
+private:
+
+    std::string msg;
+
+public:
+
+    FileException(std::string m):msg(m){}
+
+    const char* what()const noexcept override{
+
+        return msg.c_str();
+    }
+
+};
+
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_FILEEXCEPTION_H
