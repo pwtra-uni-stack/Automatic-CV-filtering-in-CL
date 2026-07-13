24173decaf1fb502c403b57d8f2dc33f2f44bb15
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-13 14:52:37 +0700

    Fix score calculation type casting in AdvancedScan

--- src/scanner/AdvancedScan.cpp
+++ src/scanner/AdvancedScan.cpp
@@ -4,16 +4,13 @@ int AdvancedScan::calculateScore(const CV& cv)
 {
     int score = 0;
 
-    score += cv.skills.size() * 10;
-
+    score += static_cast<int>(cv.skills.size()) * 10;
     score += cv.experience_years * 5;
 
     if (cv.education == "Bachelor")
         score += 20;
-
     else if (cv.education == "Master")
         score += 30;
-
     else if (cv.education == "PhD")
         score += 40;
 
