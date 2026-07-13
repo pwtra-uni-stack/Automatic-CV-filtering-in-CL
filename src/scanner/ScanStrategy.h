65bd9616760d9e54539543b09c51516e9fe34222
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-13 14:53:20 +0700

    Update ScanStrategy.h

--- src/scanner/ScanStrategy.h
+++ src/scanner/ScanStrategy.h
@@ -4,13 +4,15 @@
 
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_SCANSTRATEGY_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_SCANSTRATEGY_H
+
 #include "../model/CV.h"
 
-
-class ScanStrategy {
+class ScanStrategy
+{
 public:
-    virtual bool scan(const CV& cv)=0;
-    virtual ~ScanStrategy(){}
+    virtual bool scan(const CV& cv) = 0;
+    virtual ~ScanStrategy() = default;
 };
+
 
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_SCANSTRATEGY_H
