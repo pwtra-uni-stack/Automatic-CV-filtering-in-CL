5c79fad3919b0130924d9bbca2aaa1ca1d89878f
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-13 23:17:33 +0700

    Fix include directive formatting in AdvancedScan.h

--- src/scanner/AdvancedScan.h
+++ src/scanner/AdvancedScan.h
@@ -5,15 +5,17 @@
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_ADVANCEDSCAN_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_ADVANCEDSCAN_H
 
-#include "ScanStrategy.h"
+include "ScanStrategy.h"
 
 class AdvancedScan : public ScanStrategy
 {
+private:
+    bool hasRequiredSkill(const CV& cv);
+    bool hasEnoughExperience(const CV& cv);
+    bool hasEnoughScore(const CV& cv);
+
 public:
     bool scan(const CV& cv) override;
-
-private:
-    int calculateScore(const CV& cv);
 };
 
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_ADVANCEDSCAN_H
