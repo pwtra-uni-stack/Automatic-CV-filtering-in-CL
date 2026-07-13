376583b036b40f56c685055115640a3e8b3a3b20
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-13 23:19:20 +0700

    Refactor ScanEngine to use ScanResult struct

--- src/scanner/ScanEngine.h
+++ src/scanner/ScanEngine.h
@@ -6,29 +6,32 @@
 #define AUTOMATIC_CV_FILTERING_IN_CL_SCANENGINE_H
 
 #include <vector>
-#include "../model/CVDatabase.h"
-#include "BasicScan.h"
-#include "AdvancedScan.h"
 
-class ScanEngine {
+#include "../model/CV.h"
+#include "ScanStrategy.h"
+
+struct ScanResult
+{
+    std::vector<CV*> junk;
+    std::vector<CV*> mismatch;
+    std::vector<CV*> qualified;
+};
+
+class ScanEngine
+{
 private:
-    BasicScan basicScan;
-    AdvancedScan advancedScan;
-
-    std::vector<CV*> junkList;
-    std::vector<CV*> mismatchList;
-    std::vector<CV*> qualifiedList;
+    std::vector<ScanStrategy*> strategies;
 
 public:
     ScanEngine();
 
-    void scan(CVDatabase& database);
+    void addStrategy(ScanStrategy* strategy);
 
-    const std::vector<CV*>& getJunk() const;
-    const std::vector<CV*>& getMismatch() const;
-    const std::vector<CV*>& getQualified() const;
+    void clearStrategies();
 
-    void clear();
+    ScanResult scanAll(const std::vector<CV*>& cvs);
 };
+
+
 
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_SCANENGINE_H
