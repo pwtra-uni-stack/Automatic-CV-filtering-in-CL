8a052e8e0194a3a7cba37a5f95a28856e3384b6e
chunguyeny-stack <chunguyeny@gmail.com>
2026-07-13 14:51:47 +0700

    Refactor AdvancedScan class structure
    Refactor AdvancedScan class to move calculateScore to private.

--- src/scanner/AdvancedScan.h
+++ src/scanner/AdvancedScan.h
@@ -4,39 +4,16 @@
 
 #ifndef AUTOMATIC_CV_FILTERING_IN_CL_ADVANCEDSCAN_H
 #define AUTOMATIC_CV_FILTERING_IN_CL_ADVANCEDSCAN_H
+
 #include "ScanStrategy.h"
 
-class AdvancedScan : public ScanStrategy{
-
+class AdvancedScan : public ScanStrategy
+{
 public:
+    bool scan(const CV& cv) override;
 
-    int calculateScore(const CV& cv){
-
-        int score=0;
-
-        score+=cv.skills.size()*10;
-
-        score+=cv.experience_years*5;
-
-        if(cv.education=="Bachelor")
-            score+=20;
-
-        else if(cv.education=="Master")
-            score+=30;
-
-        else if(cv.education=="PhD")
-            score+=40;
-
-        return score;
-    }
-
-    bool scan(const CV& cv) override{
-
-        int score=calculateScore(cv);
-
-        return score>=60;
-    }
-
+private:
+    int calculateScore(const CV& cv);
 };
 
 #endif //AUTOMATIC_CV_FILTERING_IN_CL_ADVANCEDSCAN_H
